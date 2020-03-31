32754c43f18d3dcb0a7b7bb3d5f64ac00acb2fa3
valentincodage08 <valentin_mg08@hotmail.fr>
2020-03-31 17:01:05 +0200

    commit mardi

--- include/connexiondb.php
+++ include/connexiondb.php
@@ -1,9 +1,11 @@
 <?php
+
+$servername = "db5000303630.hosting-data.io";
+$username = "dbu526536";
+$password = "7f,7]WCg";
+$dbname = "dbs296617";
+
 try   {
-    $servername = "db5000303630.hosting-data.io";
-    $username = "dbu526536";
-    $password = "7f,7]WCg";
-    $dbname = "dbs296617";
   // Je me connecte à ma bdd
   $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   return $bdd;
@@ -12,8 +14,5 @@ try   {
   // En cas d'erreur, un message s'affiche et tout s'arrête
         die('Erreur : '.$e->getMessage());
 }
-
-echo 'Ca marche';
-
 
 ?>
--- include/films.php
+++ include/films.php
@@ -1,154 +1,32 @@
 <!--CATALOGUE FILMS-->
-        
+
+<?php
+
+include 'connexiondb.php';
+//$req = $bdd->prepare(" SELECT id_film, titre, note, duree, genre, Film.id_affiche FROM Film INNER JOIN AfficheFilm ON Film.id_affiche = AfficheFilm.image");//
+$req = $bdd->prepare(" SELECT id_film, titre, note, duree, genre, Film.id_affiche, image FROM Film, AfficheFilm");
+          $req ->execute();
+
+while( $donnees = $req->fetch() ) {
+
+?>
+
 <div class="axeldroite">
-            <a href="parasite.php" class="versfilm">
-            <div class="cardaxel">
-                <img class="poster-img" src="./img/poster1.jpg" alt="">
-                <div class="titrefilm">Parasite</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                       <p>4.5/5</p> 
-                       <p>2h12</p> 
-                       <p>Thriller</p>    
-                    </div>
+    <a href="<?php $_GET['id_film']?>" class="versfilm">
+        <div class="cardaxel">
+            <img class="poster-img" src="<?= $donnees['image']; ?>" alt="">
+            <div class="titrefilm"><?= $donnees['titre']; ?></div>
+            <div class="infoaxel">
+                <div class="textaxel">
+                    <p><?= $donnees['note']; ?>/5</p>
+                    <p><?= $donnees['duree']; ?></p>
+                    <p><?= $donnees['genre']; ?></p>
                 </div>
             </div>
-            </a>
-            <a href="" class="versfilm">
-            <div class="cardaxel">
-                <img class="poster-img" src="./img/poster2.jpg" alt="">
-                <div class="titrefilm">The Irishman</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                        <p>3.3/5</p> 
-                        <p>3h29</p> 
-                        <p>Thriller, Biopic</p>   
-                    </div>
-                </div>
-            </div>
-            </a>
-            <a href="" class="versfilm">
-            <div class="cardaxel">
-                <img class="poster-img" src="./img/poster3.jpg" alt="">
-                <div class="titrefilm">Vivre et chanter</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                        <p>3.3/5</p> 
-                        <p>1h45</p> 
-                        <p>Drame</p>   
-                    </div>
-                </div>
-            </div>
-            </a>
-            <a href="" class="versfilm">
-            <div class="cardaxel">
-                <img class="poster-img" src="./img/poster4.jpg" alt="">
-                <div class="titrefilm">Dark Waters</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                        <p>3.9/5</p> 
-                        <p>2h08</p> 
-                        <p>Biopic,Drame</p>   
-                    </div>
-                </div>
-            </div>
-            </a>
-
-            <a href="" class="versfilm">
-                <div class="cardaxel">
-                <img class="poster-img" src="./img/poster5.jpg" alt="">
-                <div class="titrefilm">En Avant</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                        <p>3.7/5</p> 
-                       <p>1h42</p> 
-                       <p>Animation, Fantastique</p>   
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a href="" class="versfilm">
-            <div class="cardaxel">
-                <img class="poster-img" src="./img/poster6.jpg" alt="">
-                <div class="titrefilm">Le cas de Richard Jewell</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                        <p>4/5</p> 
-                       <p>2h09</p> 
-                       <p>Drame</p>   
-                    </div>
-                </div>
-            </div>
-            </a>
-            <a href="" class="versfilm">
-            <div class="cardaxel">
-                <img class="poster-img" src="./img/poster7.jpg" alt="">
-                <div class="titrefilm">De Gaulle</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                        <p>2.9/5</p> 
-                        <p>1h49</p> 
-                        <p>Historique, Biopic, Guerre</p>   
-                    </div>
-                </div>
-            </div>
-            </a>
-            <a href="" class="versfilm">
-            <div class="cardaxel">
-                <img class="poster-img" src="./img/poster8.jpg" alt="">
-                <div class="titrefilm">L'appel de la forêt</div>
-                <div class="infoaxel">
-                    <div class="textaxel">
-                        <p>3.2/5</p> 
-                       <p>1h40</p> 
-                       <p>Aventure, Drame, Famille</p>   
-                    </div>
-                </div>
-            </div>
-            </a>
-            <a href="" class="versfilm">
-                <div class="cardaxel">
-                    <img class="poster-img" src="./img/poster11.jpg" alt="">
-                    <div class="titrefilm">(500) Jours ensemble</div>
-                    <div class="infoaxel">
-                        <div class="textaxel">
-                            <p>3.3/5</p> 
-                           <p>1h36</p> 
-                           <p>Comédie, Romance</p>   
-                        </div>
-                    </div>
-                </div>
-                </a>
-
-                <a href="" class="versfilm">
-                    <div class="cardaxel">
-                        <img class="poster-img" src="./img/poster10.jpg" alt="">
-                        <div class="titrefilm">Midsommar</div>
-                        <div class="infoaxel">
-                            <div class="textaxel">
-                                <p>3.7/5</p> 
-                               <p>2h27</p> 
-                               <p>Horreur</p>   
-                            </div>
-                        </div>
-                    </div>
-                    </a>
-
-                    <a href="" class="versfilm">
-                        <div class="cardaxel">
-                            <img class="poster-img" src="./img/poster9.jpg" alt="">
-                            <div class="titrefilm">Fast & Furious : Hobbs & Shaw</div>
-                            <div class="infoaxel">
-                                <div class="textaxel">
-                                    <p>2.8/5</p> 
-                                   <p>2h16</p> 
-                                   <p>Action</p>   
-                                </div>
-                            </div>
-                        </div>
-                        </a>
-
         </div>
-
-
-    </div>
+    </a>
+</div>
+</div>
+<?php
+} $req->closecursor();
+?>
